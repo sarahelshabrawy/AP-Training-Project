55b1b0ce95513c80d567f97d17043f9496c22dae
AlyZaitoon <74425805+AlyZaitoon@users.noreply.github.com>
2020-11-22 16:01:57 +0200

    second version

---
+++ new version/Motion_Map.py
@@ -0,0 +1,193 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import *
+import cv2
+import numpy as np
+#-----------------------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------Motion map code-----------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------------------------
+class Ui_MotionMapWindow(QMainWindow):
+
+    def setupUi(self):
+        self.setWindowTitle("Motion Map")
+        self.setObjectName("MotionMapWindow")
+        self.resize(483, 607)
+        self.centralwidget = QtWidgets.QWidget()
+        self.centralwidget.setObjectName("centralwidget")
+        self.motionMapLabel = QtWidgets.QLabel(self.centralwidget)
+        self.motionMapLabel.setGeometry(QtCore.QRect(20, 20, 441, 501))
+        self.motionMapLabel.setFrameShape(QtWidgets.QFrame.Box)
+        self.motionMapLabel.setLineWidth(3)
+        self.motionMapLabel.setScaledContents(True)
+        self.motionMapLabel.setText("")
+        self.motionMapLabel.setObjectName("motionMapLabel")
+        self.rMotorLabel = QtWidgets.QLabel("R-Motor: Clockwise", self.centralwidget)
+        self.rMotorLabel.setGeometry(QtCore.QRect(30, 530, 201, 31))
+        font = QtGui.QFont()
+        font.setPointSize(9)
+        font.setBold(True)
+        font.setWeight(75)
+        self.rMotorLabel.setFont(font)
+        self.rMotorLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.rMotorLabel.setObjectName("rMotorLabel")
+        self.lMotorLabel = QtWidgets.QLabel("L-Motor: Clockwise", self.centralwidget)
+        self.lMotorLabel.setGeometry(QtCore.QRect(230, 530, 201, 31))
+        font = QtGui.QFont()
+        font.setPointSize(9)
+        font.setBold(True)
+        font.setWeight(75)
+        self.lMotorLabel.setFont(font)
+        self.lMotorLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.lMotorLabel.setObjectName("lMotorLabel")
+        self.speedLabel = QtWidgets.QLabel("Current Speed: Lowest", self.centralwidget)
+        self.speedLabel.setGeometry(QtCore.QRect(30, 560, 221, 31))
+        font = QtGui.QFont()
+        font.setPointSize(9)
+        font.setBold(True)
+        font.setWeight(75)
+        self.speedLabel.setFont(font)
+        self.speedLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.speedLabel.setObjectName("speedLabel")
+        self.typeLabel = QtWidgets.QLabel("DC", self.centralwidget)
+        self.typeLabel.setGeometry(QtCore.QRect(290, 560, 81, 31))
+        font = QtGui.QFont()
+        font.setPointSize(9)
+        font.setBold(True)
+        font.setWeight(75)
+        self.typeLabel.setFont(font)
+        self.typeLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.typeLabel.setObjectName("typeLabel")
+        self.setCentralWidget(self.centralwidget)
+        self.map = np.zeros((500,500,3), np.uint8) #create a black background for the motion map, by creating a numpy array filled with zeros
+        self.map = cv2.circle(self.map, (250, 480), radius=3, color=(238, 130, 238), thickness=-1) #draw a point in the starting point
+        qtMap = QtGui.QImage(self.map.data, self.map.shape[1], self.map.shape[0], QtGui.QImage.Format_RGB888).rgbSwapped() #convert the image to be used with qt label
+        pixmap = QtGui.QPixmap.fromImage(qtMap)
+        self.motionMapLabel.setPixmap(pixmap)
+        self.x = 250
+        self.y = 480
+
+        self.motorType = "DC"
+
+#---------------------------------------------------------Variables to send to the Arduino-----------------------------------------------------------
+        self.motorDirData = "CC"
+        self.motorSpeedData = "L"
+        self.motorTypeData = "D"
+
+#---------------------------------------------------------Variables to send to the MainWindow-----------------------------------------------------------
+        Ui_MotionMapWindow.rMotorDirection = "Clockwise"
+        Ui_MotionMapWindow.lMotorDirection = "Clockwise"
+        Ui_MotionMapWindow.motorSpeed = "Lowest"
+
+#--------------------------------------------------------function to update the motion map-----------------------------------------------------------
+    def update_map(self):
+        self.map = cv2.circle(self.map, (self.x, self.y), radius=3, color=(238, 130, 238), thickness=-1) #draw a point in the starting point, thickness -1 means a filled circle
+        qtMap = QtGui.QImage(self.map.data, self.map.shape[1], self.map.shape[0], QtGui.QImage.Format_RGB888).rgbSwapped() #convert the image to be used with qt label
+        pixmap = QtGui.QPixmap.fromImage(qtMap)
+        self.motionMapLabel.setPixmap(pixmap)
+
+#------------------------------------------------------the responses to different key presses---------------------------------------------------------
+    def up_response(self):
+        self.y -= 3
+        self.update_map()
+        self.lMotorLabel.setText("L-Motor: Clockwise")
+        self.rMotorLabel.setText("R-Motor: Clockwise")
+        self.motorDirData = "CC"
+        Ui_MotionMapWindow.rMotorDirection = "Clockwise"
+        Ui_MotionMapWindow.lMotorDirection = "Clockwise"
+
+
+    def down_response(self):
+        self.y += 3
+        self.update_map()
+        self.lMotorLabel.setText("L-Motor: Anti-clockwise")
+        self.rMotorLabel.setText("R-Motor: Anti-clockwise")
+        self.motorDirData = "AA"
+        Ui_MotionMapWindow.rMotorDirection = "Anti-clockwise"
+        Ui_MotionMapWindow.lMotorDirection = "Anti-clockwise"
+
+    def left_response(self):
+        self.x -= 3
+        self.update_map()
+        self.lMotorLabel.setText("L-Motor: Stopped")
+        self.rMotorLabel.setText("R-Motor: Clockwise")
+        self.motorDirData = "SC"
+        Ui_MotionMapWindow.rMotorDirection = "Clockwise"
+        Ui_MotionMapWindow.lMotorDirection = "Stopped"
+
+    def right_response(self):
+        self.x += 3
+        self.update_map()
+        self.lMotorLabel.setText("L-Motor: Clockwise")
+        self.rMotorLabel.setText("R-Motor: Stopped")
+        self.motorDirData = "CS"
+        Ui_MotionMapWindow.rMotorDirection = "Stopped"
+        Ui_MotionMapWindow.lMotorDirection = "Clockwise"
+
+    def l_response(self):
+        self.speedLabel.setText("Current Speed: Lowest")
+        self.motorSpeedData = "L"
+        self.motorSpeed = "Lowest"
+
+    def m_response(self):
+        self.speedLabel.setText("Current Speed: Medium")
+        self.motorSpeedData = "M"
+        self.motorSpeed = "Medium"
+
+    def h_response(self):
+        self.speedLabel.setText("Current Speed: Highest")
+        self.motorSpeedData = "H"
+        self.motorSpeed = "Highest"
+
+    def s_response(self): #switch between DC and BL motors
+        if self.motorType == "DC":
+            self.motorType = "BLDC"
+            self.motorTypeData = "B"
+            self.typeLabel.setText(self.motorType)
+        else:
+            self.motorType = "DC"
+            self.motorTypeData = "D"
+            self.typeLabel.setText(self.motorType)
+
+    def c_response(self):
+        self.map = np.zeros((500,500,3), np.uint8)
+        self.update_map()
+
+    def keyPressEvent(self, event): #function that detects keypresses
+        UP_KEY = 16777235
+        DOWN_KEY = 16777237
+        LEFT_KEY = 16777234
+        RIGHT_KEY = 16777236
+        L_KEY = 76
+        M_KEY = 77
+        H_KEY = 72
+        C_KEY = 67
+        S_KEY = 83
+
+        if((self.x <= 500 and self.x > 0) and (self.y <= 500 and self.y > 0)):
+
+            if event.key() == UP_KEY:
+                self.up_response()
+            elif event.key() == DOWN_KEY:
+                self.down_response()
+            elif event.key() == LEFT_KEY:
+                self.left_response()
+            elif event.key() == RIGHT_KEY:
+                self.right_response()
+            elif event.key() == L_KEY:
+                self.l_response()
+            elif event.key() == M_KEY:
+                self.m_response()
+            elif event.key() == H_KEY:
+                self.h_response()
+            elif event.key() == S_KEY:
+                self.s_response()
+            elif event.key() == C_KEY: #if c key is pressed, clear the map
+                self.c_response()
+
+            else:
+                print("Invalid Input")
+
+        else: #reset the map to the starting point
+            self.x = 250
+            self.y = 480
+
+
---
+++ new version/Spot_the_diff.py
@@ -0,0 +1,173 @@
+#---------------------------------------------------------------Libraries----------------------------------------------------------------
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtCore import QTimer, QThread
+from PyQt5.QtGui import QImage
+from PyQt5.QtWidgets import *
+import cv2
+import imutils as imutils
+from skimage.measure import compare_ssim
+import numpy as np
+from PIL import Image,ImageChops
+MAX_FEATURES =500
+GOOD_MATCH_PERCENT = 0.15
+#-----------------------------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------Spot the difference window code--------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------------------------
+
+class Ui_DifferencesWindow(QMainWindow):
+    def setupUi(self):
+        self.setObjectName("DifferencesWindow")
+        self.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget()
+        self.centralwidget.setObjectName("centralwidget")
+        self.picture1 = QtWidgets.QLabel(self.centralwidget)
+        self.picture1.setGeometry(QtCore.QRect(80, 140, 251, 351))
+        self.picture1.setFrameShape(QtWidgets.QFrame.Box)
+        self.picture1.setLineWidth(3)
+        self.picture1.setScaledContents(True)
+        self.picture1.setObjectName("picture1")
+        self.picture2 = QtWidgets.QLabel(self.centralwidget)
+        self.picture2.setGeometry(QtCore.QRect(450, 140, 251, 351))
+        self.picture2.setFrameShape(QtWidgets.QFrame.Box)
+        self.picture2.setLineWidth(3)
+        self.picture2.setScaledContents(True)
+        self.picture2.setObjectName("picture2")
+        self.title = QtWidgets.QLabel("<html><head/><body><p><span style=\" font-size:16pt; font-weight:600;\">Spot the differences Game</span></p></body></html>", self.centralwidget)
+        self.title.setGeometry(QtCore.QRect(200, 40, 371, 81))
+        self.title.setObjectName("title")
+        self.picture1_btn = QtWidgets.QPushButton("Picture 1", self.centralwidget)
+        self.picture1_btn.setGeometry(QtCore.QRect(140, 510, 93, 28))
+        self.picture1_btn.setObjectName("picture1_btn")
+        self.picture2_btn = QtWidgets.QPushButton("Picture 2", self.centralwidget)
+        self.picture2_btn.setGeometry(QtCore.QRect(520, 510, 93, 28))
+        self.picture2_btn.setObjectName("picture2_btn")
+        self.spotdiff_btn = QtWidgets.QPushButton("Spot Differences", self.centralwidget)
+        self.spotdiff_btn.setGeometry(QtCore.QRect(320, 510, 131, 28))
+        self.spotdiff_btn.setObjectName("spotdiff_btn")
+        self.number = QtWidgets.QLabel("<html><head/><body><p><span style=\" font-size:9pt;\">Number of differnces </span></p></body></html>", self.centralwidget)
+        self.number.setGeometry(QtCore.QRect(250, 550, 151, 21))
+        self.number.setObjectName("number")
+        self.numbervalue = QtWidgets.QLabel("<html><head/><body><p><span style=\" font-size:9pt; font-weight:600;\">0</span></p></body></html>", self.centralwidget)
+        self.numbervalue.setGeometry(QtCore.QRect(410, 550, 91, 21))
+        self.numbervalue.setObjectName("numbervalue")
+        self.setCentralWidget(self.centralwidget)
+        self.im1 = None
+        self.im2 = None
+        self.picture1_btn.clicked.connect(self.readpic1)
+        self.picture2_btn.clicked.connect(self.readpic2)
+        self.spotdiff_btn.clicked.connect(self.spot_diff)
+        self.differences = 0
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+    def alignImages(self,im1, im2):
+        # Convert images to grayscale
+        im1Gray = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+        im2Gray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+
+        # Detect ORB features and compute descriptors.
+        orb = cv2.ORB_create(MAX_FEATURES)
+        keypoints1, descriptors1 = orb.detectAndCompute(im1Gray, None)
+        keypoints2, descriptors2 = orb.detectAndCompute(im2Gray, None)
+
+        # Match features.
+        matcher = cv2.DescriptorMatcher_create(cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
+        matches = matcher.match(descriptors1, descriptors2, None)
+
+        # Sort matches by score
+        matches.sort(key=lambda x: x.distance, reverse=False)
+
+        # Remove not so good matches
+        numGoodMatches = int(len(matches) * GOOD_MATCH_PERCENT)
+        matches = matches[:numGoodMatches]
+
+        # Draw top matches
+        imMatches = cv2.drawMatches(im1, keypoints1, im2, keypoints2, matches, None)
+        cv2.imwrite("matches.jpg", imMatches)
+
+        # Extract location of good matches
+        points1 = np.zeros((len(matches), 2), dtype=np.float32)
+        points2 = np.zeros((len(matches), 2), dtype=np.float32)
+
+        for i, match in enumerate(matches):
+            points1[i, :] = keypoints1[match.queryIdx].pt
+            points2[i, :] = keypoints2[match.trainIdx].pt
+
+        # Find homography
+        h, mask = cv2.findHomography(points1, points2, cv2.RANSAC)
+
+        # Use homography
+        height, width, channels = im2.shape
+        im1Reg = cv2.warpPerspective(im1, h, (width, height))
+
+        return im1Reg, h
+    def readpic1(self):
+        filename = QFileDialog.getOpenFileName()
+        path = filename[0]
+        self.picture1.setPixmap(QtGui.QPixmap(path))
+        self.im1 =cv2.imread(path)
+
+    def readpic2(self):
+        filename = QFileDialog.getOpenFileName()
+        path = filename[0]
+        self.picture2.setPixmap(QtGui.QPixmap(path))
+        self.im2 = cv2.imread(path)
+
+    def spot_diff(self):
+        kernel = np.array([[-1, -1, -1],
+                           [-1, 9, -1],
+                           [-1, -1, -1]])
+
+
+
+        im1,_=self.alignImages(self.im1,self.im2)
+        im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+        ksize = (3, 3)
+
+        img1 = cv2.blur(im1, ksize)
+        img1 = cv2.GaussianBlur(img1, (5,5),250)
+
+        img1 = cv2.adaptiveThreshold(img1, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+
+
+        im2 = cv2.cvtColor(self.im2, cv2.COLOR_BGR2GRAY)
+        ksize = (3, 3)
+
+        img2 = cv2.blur(im2, ksize)
+        img2 = cv2.GaussianBlur(img2, (5,5), 250)
+
+        img2 = cv2.adaptiveThreshold(img2, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+
+
+
+        (score, diff) = compare_ssim(img1, img2, full=True)
+        diff = (diff * 255).astype("uint8")
+        print("SSIM: {}".format(score))
+
+        _,thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV )
+
+        contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+
+
+
+
+        for c in contours:
+        # compute the bounding box of the contour and then draw the
+        # bounding box on both input images to represent where the two
+        # images differ
+                if cv2.contourArea(c)>500 and cv2.contourArea(c)<1000:
+                    (x, y, w, h) = cv2.boundingRect(c)
+                    cv2.rectangle(self.im1, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                    cv2.rectangle(self.im2, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                    self.differences+=1
+
+
+    # show the output images
+
+        self.numbervalue.setText(str(self.differences))
+        cv2.imshow("im1", self.im1)
+        cv2.imshow("im2", self.im2)
+        cv2.waitKey(0)
+
